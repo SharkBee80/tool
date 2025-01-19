30e3054455c7b87624cae04a22bbcb1aaa6c5920
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2025-01-19 20:49:02 +0800

    init

---
+++ funcjs/txt2m3uPage.js
@@ -0,0 +1,51 @@
+function convertToM3U(res, txtInput) {
+    const lines = txtInput.split('\n');
+    let m3uOutput = '#EXTM3U x-tvg-url="https://live.fanmingming.cn/e.xml"\n';
+    let currentGroup = null;
+    for (const line of lines) {
+        const trimmedLine = line.trim();
+        if (trimmedLine !== '') {
+            if (trimmedLine.includes('#genre#')) {
+                currentGroup = trimmedLine.replace(/,#genre#/, '').trim();
+            } else {
+                const [originalChannelName, channelLink] = trimmedLine.split(',').map(item => item.trim());
+                const processedChannelName = originalChannelName.replace(/(CCTV|CETV)-(\d+).*/, '$1$2');
+                m3uOutput += `#EXTINF:-1 tvg-name="${processedChannelName}" tvg-logo="https://live.fanmingming.cn/tv/${processedChannelName}.png"`;
+                if (currentGroup) {
+                    m3uOutput += ` group-title="${currentGroup}"`;
+                }
+                m3uOutput += `,${originalChannelName}\n${channelLink}\n`;
+            }
+        }
+    }
+    // 设置响应头并返回 M3U 数据
+    res.setHeader('Content-Disposition', 'inline; filename=txt2m3u.m3u');// 防止文件下载
+    res.setHeader('Content-Type', 'audio/x-mpegurl');
+    //res.setHeader('Content-Type', 'text/plain');
+    res.send(m3uOutput)
+}
+
+async function fetchTextFile(res, url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`请求失败，状态码：${response.status}`);
+        }
+        const txtInput = await response.text();
+        if (typeof txtInput !== 'string') {
+            throw new Error('Response is not a string');
+        }
+        // 在这里处理 txtInput，例如调用 convertToM3U(txtInput)
+        convertToM3U(res, txtInput)
+    } catch (error) {
+        console.error('获取文件时出错：', error);
+        res.send('获取文件时出错：' + error)
+        return
+    }
+}
+
+function txt2m3uPage(res, url) {
+    txtInput = fetchTextFile(res, url)
+}
+
+module.exports = txt2m3uPage;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const txt2m3uPage = require('./funcjs/txt2m3uPage');
+
+const app = express();
+const port = 4096;
+
+// 设置静态文件目录
+app.use(express.static('tools'));
+app.use(express.static('funcjs'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/tools/index.html');
+});
+
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + '/tools/index.html');
+  });
+
+app.get('/txt2m3u', async (req, res) => {
+    const txtUrl = req.query.url;
+    if (!txtUrl) {
+      return res.status(400).send('缺少必要的 url 参数');
+    }
+    txt2m3uPage(res,txtUrl)
+});
+
+app.listen(port, () => {
+  console.log(`静态服务器已启动，访问地址：http://localhost:${port}`);
+});
