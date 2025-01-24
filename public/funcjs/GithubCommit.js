491a320a44adf0f75d6e95a73a12c0aeed56b6aa
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2025-01-24 20:51:09 +0800

    update-time

--- public/funcjs/GithubCommit.js
+++ public/funcjs/GithubCommit.js
@@ -1,14 +1,33 @@
 const axios = require('axios');
+const { createCanvas } = require('canvas');
+
+// 自动换行
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+    const words = text.split('\n');
+    let line = '';
+    for (let i = 0; i < words.length; i++) {
+        const testLine = line + words[i] + ' ';
+        const testWidth = context.measureText(testLine).width;
+        if (testWidth > maxWidth && i > 0) {
+            context.fillText(line, x, y);
+            line = words[i] + ' ';
+            y += lineHeight;
+        } else {
+            line = testLine;
+        }
+    }
+    context.fillText(line, x, y);
+}
 
 async function GithubCommit(req, res) {
-    const username = req.query.user;
-    const project = req.query.proj
-    const style = "style=background-color:#151b23;color:white;"
-    if (!username || !project) {
-        return res.status(400).send(`<body ${style}>缺少必要的 url 参数<br>user或proj</body>`);
+    const owner = req.query.owner; // 仓库所有者
+    const repo = req.query.repo; // 仓库名称
+    let text = "result";
+    if (!owner || !repo) {
+        text = "缺少必要的 url 参数\nowner或repo"
     } else {
         try {
-            const url = `https://api.github.com/repos/${username}/${project}/commits`;
+            const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
             const response = await axios.get(url);
             const commits = response.data;
 
@@ -16,14 +35,44 @@ async function GithubCommit(req, res) {
                 const latestCommit = commits[0];
                 const commitName = latestCommit.commit.committer.name;
                 const commitDate = latestCommit.commit.committer.date;
-                res.send(`<body ${style}>更新人员: ${commitName}<br>更新日期: ${commitDate}</body>`);
+                text = `更新人员: ${commitName}\n更新日期: ${commitDate}`
             } else {
-                res.send(`<body ${style}>未找到信息。</body>`);
+                text = `未找到更新信息。`
             }
         } catch (error) {
-            res.status(500).send(`<body ${style}>Error fetching data: ${error}</body>`);
+            text = `Error fetching data: \n${error}`
         }
     }
+
+    // 创建画布和上下文
+    const width = 400;
+    const height = 100;
+
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+
+    // 设置背景颜色
+    ctx.fillStyle = 'rgba(255, 255, 255, 0)';
+    ctx.fillRect(0, 0, width, height);
+
+    // 设置文本样式
+    ctx.fillStyle = '#ffff00';
+    ctx.font = 'bold 24px SimHei';
+
+    // 设置对齐方式
+    ctx.textAlign = 'left'; // 左对齐
+    ctx.textBaseline = 'middle'; // 垂直居中
+
+    // 绘制文本
+    wrapText(ctx, text, 0, height / 3, width , 40);
+
+
+    // 返回图片
+    const buffer = canvas.toBuffer('image/png');
+    res.set('Content-Type', 'image/png');
+    res.send(buffer);
 }
+
+
 
 module.exports = GithubCommit;
