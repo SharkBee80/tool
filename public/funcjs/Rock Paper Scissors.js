dc9d51ff1a6d796c31f32fd43558f26f7490f4c0
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2025-02-02 09:54:03 +0800

    update

--- public/funcjs/Rock Paper Scissors.js
+++ public/funcjs/Rock Paper Scissors.js
@@ -19,12 +19,13 @@ const choices = [
 
 let playerPoints = document.querySelector(".playerPoints")
 let computerPoints = document.querySelector(".computerPoints")
+let draw = document.querySelector('.draw');
 let playerChoiceImg = document.querySelector("#playerChoiceImg")
 let playerChoiceTxt = document.querySelector("#playerChoiceTxt")
 let computerChoiceImg = document.querySelector("#computerChoiceImg")
 let computerChoiceTxt = document.querySelector("#computerChoiceTxt")
 let buttons = document.querySelectorAll(".btn")
-let points = [0, 0]
+let points = [0, 0, 0]
 let randomNumber;
 
 
@@ -50,6 +51,7 @@ buttons.forEach((button) => {
 
 // FUNCTIONS
 function getComputerChoice() {
+    buttons.forEach((button) => { button.disabled = true; button.style.pointerEvents = 'none' });
     computerChoiceImg.src = "./img/Rock Paper Scissors/gif.gif"
     setTimeout(() => {
         randomNumber = Math.floor(Math.random() * 3);
@@ -58,21 +60,29 @@ function getComputerChoice() {
         gameRules();
         playerPoints.textContent = points[0];
         computerPoints.textContent = points[1];
+        draw.textContent = points[2];
         whoWon();
+        buttons.forEach((button) => { button.disabled = false; button.style.pointerEvents = 'auto' });
     }, 1000);
 }
 
 function gameRules() {
     if (playerChoiceTxt.textContent === choices[0].name && computerChoiceTxt.textContent === choices[1].name) {
         points[1]++;
+        noty('Lose!');
     } else if (playerChoiceTxt.textContent === choices[1].name && computerChoiceTxt.textContent === choices[2].name) {
         points[1]++;
+        noty('Lose!');
     } else if (playerChoiceTxt.textContent === choices[2].name && computerChoiceTxt.textContent === choices[0].name) {
         points[1]++;
-    } else if (playerChoiceTxt.textContent === computerChoiceTxt.textContent) {
-        console.log("draw");
+        noty('Lose!');
+    }
+    else if (playerChoiceTxt.textContent === computerChoiceTxt.textContent) {
+        points[2]++;
+        noty('平局');
     } else {
         points[0]++;
+        noty('Win!');
     }
 }
 
