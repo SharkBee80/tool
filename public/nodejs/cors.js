e4654395829ec5c662b6a4449e5d9ba2b0b7f46e
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2025-02-13 17:57:46 +0800

    -

--- public/nodejs/cors.js
+++ public/nodejs/cors.js
@@ -123,7 +123,12 @@ async function mode1(req, res) {
         //const m3u8Name = req.params.path || getFileName(m3u8Url);
         //const m3u8Path = path.join(CACHE_DIR, m3u8Name);
 
-        const host = `${req.protocol}://${req.get('host')}`;
+        let host = req.get('host');
+        if (host.includes('localhost','127.0.0.1')) {
+            host = 'http://' + host;
+        } else {
+            host = 'https://' + host;
+        }
 
         // 替换 m3u8 中的片段 URL，使其通过服务器代理
         m3u8Content = m3u8Content.replace(/(https?:\/\/.*?\/)(.*?\.(ts|aac|mp4|webm|m4s|m4a))/gi, (match, baseUrl, segmentPath) => {
