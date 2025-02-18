2eb814b6b9ea8c9c1a46dd0d84ef601e3dae60ea
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2025-02-19 00:29:48 +0800

    Update bcrypt dependency and remove unused import

--- nodejs/auth.js
+++ nodejs/auth.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
+//const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 //const config = require('./config');
 const path = require('path');
